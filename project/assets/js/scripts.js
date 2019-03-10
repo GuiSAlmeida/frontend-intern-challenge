6c074c9c143cd9eb8517c5815a9d7fea9ac8a05c
Gui Almeida <ga.designer.art@gmail.com>
2019-03-10 19:05:21 -0300

    Landing page refatorando - Teste estágio Linx - Guilherme Almeida

---
+++ project/assets/js/scripts.js
@@ -0,0 +1,27 @@
+// Botão simulando cópia do link
+
+document.getElementById("links_btn").onclick = function() {
+    let buttonText = document.getElementById("links_btn").innerHTML;
+    if (buttonText == "Encurtar") {
+        document.getElementById("links_text").value = "http://chr.dc/xyzxyz";
+        document.getElementById("links_text").style.color = "white";
+        document.getElementById("links_btn").innerHTML = "Copiar";
+        document.getElementById("links_close").style.display = "inline-block";
+
+    } else {
+        document.getElementById("links_text").value = "";
+        document.getElementById("links_text").style.color = "var(--secondary)";
+        document.getElementById("links_btn").innerHTML = "Encurtar";
+        document.getElementById("links_close").style.display = "none";
+        alert("Link copiado!")
+    }
+}
+
+// Botão "X" para apagar link
+
+document.getElementById("links_close").onclick = function() {
+    document.getElementById("links_text").value = "";
+    document.getElementById("links_text").style.color = "var(--secondary)";
+    document.getElementById("links_btn").innerHTML = "Encurtar";
+    document.getElementById("links_close").style.display = "none";
+}
